441f39453771acb18cd487c71e3d464f3f8cf2ee
Andrey Cavalcante <andreycavalcante963@gmail.com>
2025-11-23 04:02:41 -0300

    add address in user

---
+++ src/repositories/addressRepository.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '../generated/prisma/client';
+
+const prisma = new PrismaClient();
+
+export const addressRepository = {
+      create: async (data: {
+        state: string;
+        city: string;
+        zipCode: string;
+        district: string;
+        street: string;
+        number: string;
+        complement?: string;
+      }) => {
+        return prisma.address.create({
+            data,
+        })
+      },
+    
+      findById: (id: number) => prisma.address.findUnique({ where: { id } }),
+    
+      findAll: () => prisma.address.findMany(),
+}
